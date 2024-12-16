596f445e5cac5ff3b1bce14e5d15d1ca810ab9be
Valmir Sousa Mesquita <valmirsmesquita@gmail.com>
2024-12-16 19:14:44 -0300

    novaAtualizacao3
    novaAtualizacao3

--- PicoAula1/blink/blink.c
+++ PicoAula1/blink/blink.c
@@ -5,6 +5,7 @@
  */
 
 #include "pico/stdlib.h"
+#include <stdio.h>
 
 // Pico W devices use a GPIO on the WIFI chip for the LED,
 // so when building for Pico W, CYW43_WL_GPIO_LED_PIN will be defined
@@ -13,7 +14,7 @@
 #endif
 
 #ifndef LED_DELAY_MS
-#define LED_DELAY_MS 8000
+#define LED_DELAY_MS 2000
 #endif
 
 // Perform initialisation
@@ -42,12 +43,18 @@ void pico_set_led(bool led_on) {
 }
 
 int main() {
+
+    stdio_init_all();
+
     int rc = pico_led_init();
     hard_assert(rc == PICO_OK);
     while (true) {
         pico_set_led(true);
         sleep_ms(LED_DELAY_MS);
+        printf("LED LIGADO >>>>>>");
+
         pico_set_led(false);
         sleep_ms(LED_DELAY_MS);
+        printf("LED DESLIGADO <<<<<<");
     }
 }
